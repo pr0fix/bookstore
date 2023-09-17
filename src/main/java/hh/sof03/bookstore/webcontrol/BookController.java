90c0db8eee3a7675a477cf2e881dad56b8443d94
pr0fix <bgx152@myy.haaga-helia.fi>
2023-09-17 19:42:43 +0300

    Fourth commit

--- src/main/java/hh/sof03/bookstore/webcontrol/BookController.java
+++ src/main/java/hh/sof03/bookstore/webcontrol/BookController.java
@@ -3,9 +3,11 @@ package hh.sof03.bookstore.webcontrol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.domain.BookRepository;
 
 @Controller
@@ -23,6 +25,24 @@ public class BookController {
     public String listBooks(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
+    }
+
+    @RequestMapping(value = "/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return ("addbook");
+    };
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book) {
+        bookRepository.save(book);
+        return "redirect:booklist";
+    }
+
+    @RequestMapping(value="/delete/{id}", method = RequestMethod.GET) 
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        bookRepository.deleteById(bookId);
+        return "redirect:../booklist";
     }
 
 }
