16955b70dd6e83df4a99e4bda31c93db041c9e9a
pr0fix <bgx152@myy.haaga-helia.fi>
2023-09-27 11:58:18 +0300

    Revert "Twelfth commit"
    This reverts commit 1f4b723a2e2959c91996a242319db60658fe6403.

--- src/main/java/hh/sof03/bookstore/webcontrol/BookController.java
+++ src/main/java/hh/sof03/bookstore/webcontrol/BookController.java
@@ -18,35 +18,40 @@ public class BookController {
     @Autowired
     BookRepository bookRepository;
 
-
     @Autowired
     CategoryRepository categoryRepository;
 
+    // Lists all the books in booklist
     @RequestMapping(value = "/booklist", method = RequestMethod.GET)
     public String listBooks(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
     }
 
-    @RequestMapping(value = "/add")
+    // Adds a book into booklist
+    @RequestMapping(value = "/addbook")
     public String addBook(Model model) {
         model.addAttribute("book", new Book());
         model.addAttribute("categories", categoryRepository.findAll());
         return ("addbook");
     };
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String save(Book book) {
+
+    // Saves added/edited book in booklist
+    @RequestMapping(value = "/savebook", method = RequestMethod.POST)
+    public String saveBook(Book book) {
         bookRepository.save(book);
         return "redirect:booklist";
     }
 
+    // Deletes a book from booklist using id
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
         bookRepository.deleteById(bookId);
         return "redirect:../booklist";
     }
 
+    // Edits a book in booklist using id
     @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
     public String showModBook(@PathVariable("id") Long bookId, Model model) {
         model.addAttribute(("book"), bookRepository.findById(bookId));
