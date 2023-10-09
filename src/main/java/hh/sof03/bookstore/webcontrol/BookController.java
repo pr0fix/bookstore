c77c31e1e69aec6f538207505895c301c343f26e
pr0fix <bgx152@myy.haaga-helia.fi>
2023-10-09 10:24:17 +0300

    Added login-feature with two roles

--- src/main/java/hh/sof03/bookstore/webcontrol/BookController.java
+++ src/main/java/hh/sof03/bookstore/webcontrol/BookController.java
@@ -1,17 +1,24 @@
 package hh.sof03.bookstore.webcontrol;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.domain.BookRepository;
 import hh.sof03.bookstore.domain.CategoryRepository;
 
-
+@CrossOrigin
 @Controller
 public class BookController {
 
@@ -36,7 +43,6 @@ public class BookController {
         return ("addbook");
     };
 
-
     // Saves added/edited book in booklist
     @RequestMapping(value = "/savebook", method = RequestMethod.POST)
     public String saveBook(Book book) {
@@ -46,6 +52,7 @@ public class BookController {
 
     // Deletes a book from booklist using id
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasRole('ADMIN')")
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
         bookRepository.deleteById(bookId);
         return "redirect:../booklist";
@@ -57,5 +64,26 @@ public class BookController {
         model.addAttribute(("book"), bookRepository.findById(bookId));
         model.addAttribute("categories", categoryRepository.findAll());
         return "editbook";
+    }
+
+    // Method for creating login-page
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+
+    //REST Service methods:
+
+    //REST Service that lists all the books in booklist
+    @GetMapping("/books")
+    public @ResponseBody List<Book> bookListRest() {
+        return (List<Book>) bookRepository.findAll();
+    }
+
+    //Rest service to find a book by id
+    @GetMapping("/books/{id}")
+    public @ResponseBody Optional<Book> findBookByIdRest(@PathVariable("id") Long bookId) {
+        return bookRepository.findById(bookId);
     }
 }
